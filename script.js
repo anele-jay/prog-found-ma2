0ebff8ee9abcb1c84d65764191d66cb433ac39dc
anele-jay <60442392+anele-jay@users.noreply.github.com>
2020-03-15 21:28:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+//one
+var petArray = ["cat","cow","dog"];
+//two
+console.log(petArray[1]);
+//three
+petArray.push("sheep");
+//four
+console.log(petArray.length);
+//five
+var catObject = {
+    name:"Dennis",
+    colour:"brown",
+    age:7,
+};
+//six
+console.log(catObject.age);
+
+//seven
+var catArray = [
+{
+name:"Dennis",
+colour:"brown",
+age:7
+}
+]
+//eight
+for(i = 0; i < catArray.length; i++);{
+    console.log(i);
+}
+//nine
+function logToConsole(pumba) {
+    console.log(pumba);
+}
+//ten
+logToConsole("donkey");
